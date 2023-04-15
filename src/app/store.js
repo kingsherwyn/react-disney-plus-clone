5e54b9e8305ebb23610ed68c17246a7ccf97071e
Sherwyn Augustus <s.augustus@me.com>
2023-04-15 12:12:29 -0700

    Delete All

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import movieReducer from '../features/counter/movie/movieSlice'
-
-export const store = configureStore({
-  reducer: {
-    movie: movieReducer
-  },
-});
